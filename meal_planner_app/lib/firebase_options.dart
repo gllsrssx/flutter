572cab43db610cf66ad516b1c70f2ceb4328c2cf
gilles20393 <gilles1996@outlook.com>
2024-06-13 01:16:07 +0200

    chore: Refactor firebase_options.dart for better code organization

--- meal_planner_app/lib/firebase_options.dart
+++ meal_planner_app/lib/firebase_options.dart
@@ -1,23 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 
-const firebaseConfig = {
-  "apiKey": "AIzaSyDbmr4sEOYtDxPXE8smthQhgoSILtF-uHw",
-  "authDomain": "flutter-b2483.firebaseapp.com",
-  "projectId": "flutter-b2483",
-  "storageBucket": "flutter-b2483.appspot.com",
-  "messagingSenderId": "702932366935",
-  "appId": "1:702932366935:web:495ee10a05275313c55512"
-};
-
-Future<void> initializeFirebase() async {
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: firebaseConfig['apiKey']!,
-      authDomain: firebaseConfig['authDomain']!,
-      projectId: firebaseConfig['projectId']!,
-      storageBucket: firebaseConfig['storageBucket']!,
-      messagingSenderId: firebaseConfig['messagingSenderId']!,
-      appId: firebaseConfig['appId']!,
-    ),
-  );
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyDbmr4sEOYtDxPXE8smthQhgoSILtF-uHw",
+      authDomain: "flutter-b2483.firebaseapp.com",
+      projectId: "flutter-b2483",
+      storageBucket: "flutter-b2483.appspot.com",
+      messagingSenderId: "702932366935",
+      appId: "1:702932366935:web:495ee10a05275313c55512",
+    );
+  }
 }
