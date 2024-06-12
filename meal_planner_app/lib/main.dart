0c4b8588630530c0c55d76d47e1039b204595dfd
gilles20393 <gilles1996@outlook.com>
2024-06-13 01:16:12 +0200

    chore: Update main.dart with Firebase initialization and route configuration

--- meal_planner_app/lib/main.dart
+++ meal_planner_app/lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'login_page.dart';
+import 'register_page.dart';
+import 'home_page.dart';
+import 'profile_page.dart';
+import 'week_overview.dart';
+import 'day_detail.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeFirebase();
-  runApp(const MyApp()); // Use const
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,7 +22,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(), // Don't use const here
+      title: 'Meal Planner',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+        '/home': (context) => const HomePage(),
+        '/profile': (context) => const ProfilePage(),
+        '/week_overview': (context) => WeekOverview(startDate: DateTime.now()),
+        '/day_detail': (context) => DayDetail(date: DateTime.now()),
+      },
     );
   }
 }
