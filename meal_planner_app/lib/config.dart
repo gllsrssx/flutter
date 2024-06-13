9315781d9301445622e3dc77aa12ab513419b282
gilles20393 <gilles1996@outlook.com>
2024-06-13 02:50:14 +0200

    feat: Add Google client ID to config.dart

---
+++ meal_planner_app/lib/config.dart
@@ -0,0 +1,6 @@
+// lib/config.dart
+class Config {
+  static const String googleClientId =
+      '702932366935-bjg408ltv60m5k1auqn9dphfio58jg9n.apps.googleusercontent.com';
+  // Add other configurations here as needed
+}
