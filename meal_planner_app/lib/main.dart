53e0d70a9da984f422f1e92bbecfc9bf3c10bbf5
gilles20393 <gilles1996@outlook.com>
2024-06-13 02:50:28 +0200

    refactor: Remove login page and related code

--- meal_planner_app/lib/main.dart
+++ meal_planner_app/lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_apple/firebase_ui_oauth_apple.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'firebase_options.dart';
-import 'login_page.dart';
-import 'register_page.dart';
 import 'home_page.dart';
-import 'profile_page.dart';
 import 'week_overview.dart';
 import 'day_detail.dart';
+import 'config.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  FirebaseUIAuth.configureProviders([
+    EmailAuthProvider(),
+    PhoneAuthProvider(),
+    GoogleProvider(clientId: Config.googleClientId),
+    AppleProvider(),
+  ]);
+
   runApp(const MyApp());
 }
 
@@ -22,16 +33,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meal Planner',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/home',
       routes: {
-        '/': (context) => const LoginPage(),
-        '/register': (context) => const RegisterPage(),
+        '/sign-in': (context) {
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+              PhoneAuthProvider(),
+              GoogleProvider(clientId: Config.googleClientId),
+              AppleProvider(),
+            ],
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.pushReplacementNamed(context, '/home');
+              }),
+            ],
+          );
+        },
         '/home': (context) => const HomePage(),
-        '/profile': (context) => const ProfilePage(),
+        '/profile': (context) {
+          return ProfileScreen(
+            providers: [
+              EmailAuthProvider(),
+              PhoneAuthProvider(),
+              GoogleProvider(clientId: Config.googleClientId),
+              AppleProvider(),
+            ],
+            actions: [
+              SignedOutAction((context) {
+                Navigator.pushReplacementNamed(context, '/sign-in');
+              }),
+            ],
+          );
+        },
         '/week_overview': (context) => WeekOverview(startDate: DateTime.now()),
         '/day_detail': (context) => DayDetail(date: DateTime.now()),
       },
