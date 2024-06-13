12cefd24e7fe05e48f188da2ab8604b4bef67ac9
gilles20393 <gilles1996@outlook.com>
2024-06-13 02:50:32 +0200

    refactor: Remove login page and related code

--- meal_planner_app/lib/profile_page.dart
+++ meal_planner_app/lib/profile_page.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_apple/firebase_ui_oauth_apple.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+import 'config.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Profile Page'),
-          ],
-        ),
-      ),
+    return ProfileScreen(
+      providers: [
+        EmailAuthProvider(),
+        PhoneAuthProvider(),
+        GoogleProvider(clientId: Config.googleClientId),
+        AppleProvider(),
+      ],
+      actions: [
+        SignedOutAction((context) {
+          Navigator.pushReplacementNamed(context, '/sign-in');
+        }),
+      ],
     );
   }
 }
