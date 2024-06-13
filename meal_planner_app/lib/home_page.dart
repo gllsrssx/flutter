360e1689cfa863b0f3615fbfddda8745ce5bc69e
gilles20393 <gilles1996@outlook.com>
2024-06-13 02:50:19 +0200

    chore: Refactor home_page.dart to remove unnecessary code and improve code organization

--- meal_planner_app/lib/home_page.dart
+++ meal_planner_app/lib/home_page.dart
@@ -1,63 +1,32 @@
 import 'package:flutter/material.dart';
-import 'week_overview.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  HomePageState createState() => HomePageState();
-}
-
-class HomePageState extends State<HomePage> {
-  DateTime currentDate = DateTime.now();
-
-  void _previousWeek() {
-    setState(() {
-      currentDate = currentDate.subtract(const Duration(days: 7));
-    });
-  }
-
-  void _nextWeek() {
-    setState(() {
-      currentDate = currentDate.add(const Duration(days: 7));
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Wat eten we vandaag?'),
+        title: const Text('Home'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.arrow_back),
-                  onPressed: _previousWeek,
-                ),
-                Text(
-                  'Week van ${currentDate.day}/${currentDate.month}/${currentDate.year}',
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.arrow_forward),
-                  onPressed: _nextWeek,
-                ),
-              ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/profile');
+              },
+              child: const Text('Profile'),
             ),
-          ),
-          Expanded(
-            child: WeekOverview(
-              startDate: currentDate,
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/week_overview');
+              },
+              child: const Text('Week Overview'),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
