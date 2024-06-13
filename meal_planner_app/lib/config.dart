ed640e3c1f3e4ed95b3f1383aeca4c6a8987cbf7
gilles20393 <gilles1996@outlook.com>
2024-06-13 03:14:52 +0200

    feat: Update config.dart to use flutter_dotenv for Google client ID

--- meal_planner_app/lib/config.dart
+++ meal_planner_app/lib/config.dart
@@ -1,6 +1,8 @@
-// lib/config.dart
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class Config {
-  static const String googleClientId =
-      '702932366935-bjg408ltv60m5k1auqn9dphfio58jg9n.apps.googleusercontent.com';
-  // Add other configurations here as needed
+  static String get googleClientId =>
+      dotenv.env[
+          '702932366935-bjg408ltv60m5k1auqn9dphfio58jg9n.apps.googleusercontent.com'] ??
+      '';
 }
